d2bd5d9100f98bf60d64a86a0fbcd44f8408e0e6
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2022-03-27 11:57:13 +0200

    Initial commit

---
+++ src/main/java/com/example/convertyourmoneydkk/ConvertYourMoneyDkkApplication.java
@@ -0,0 +1,13 @@
+package com.example.convertyourmoneydkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertYourMoneyDkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertYourMoneyDkkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/convertyourmoneydkk/ConvertYourMoneyDkkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convertyourmoneydkk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertYourMoneyDkkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
